9d1c429021fde08ca27b0ed2621d07e13bc2ed20
Johnny Kamigashima <johnny@jhk.app>
2023-06-17 15:33:12 -0300

    Adicionado scrolling capture

--- cypress/e2e/project/visualtest1.cy.js
+++ cypress/e2e/project/visualtest1.cy.js
@@ -1,13 +1,20 @@
 describe('Visual testing login page', () => {
     it('visual testing- capturing base visual', () => {
-        // cy.visit('/index_v2.html')--using this url when executing 'npm run test'
+
+        if (Cypress.env('type') == null) {
+            Cypress.env('type', 'base')
+        }
         cy.visit('/index_v2.html')
+        cy.scrollTo('bottom', { ensureScrollable: false });
         cy.compareSnapshot('login-desktop', {
             capture: 'fullPage',
             errorThreshold: 0.01,
         });
         cy.viewport('ipad-2', 'portrait')
         cy.wait(200)
+        cy.scrollTo(0, 0, { ensureScrollable: false });
+        cy.wait(100)
+        cy.scrollTo('bottom', { ensureScrollable: false });
         cy.compareSnapshot('login-ipad-2', {
             capture: 'fullPage',
             errorThreshold: 0.01,
